348105ffa20435be6d1c71bd721291d70820b36c
Aya-medjeber <Abenmedjeber3714@conestogac.on.ca>
2024-10-30 21:25:20 -0400

    Main file updated. Test project properly linked

---
+++ GameTest/GameTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" char* determineWinner(const char player1[], const char player2[]);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameTest
+{
+	TEST_CLASS(GameTest)
+	{
+	public:
+		
+	
+
+	};
+}
